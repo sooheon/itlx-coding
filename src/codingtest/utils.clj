e705ba67ad98c218724ad87320e82972c22197e5
sooheon <sooheon.k@gmail.com>
2019-09-30 17:41:36 +0900

    Bring in utils

---
+++ src/codingtest/utils.clj
@@ -0,0 +1,24 @@
+(ns codingtest.utils
+  (:require [clojure.core.async :as a]))
+
+
+(defn parallelize
+  "Given a `fnc` capable of creating a transducer when called with 1-arity
+   such as `map`, `filter`, or `mapcat`, returns a fn which runs given `fnc` in
+   parallel."
+  [fnc]
+  (fn parallel-fn
+    ([concurrent f xs]
+     (let [output-chan (a/chan)]
+       (a/pipeline-blocking concurrent
+                            output-chan
+                            (fnc f)
+                            (a/to-chan xs))
+       (a/<!! (a/into [] output-chan))))
+    ([f xs] (parallel-fn (.availableProcessors (Runtime/getRuntime)) f xs))))
+
+(def pmap! (parallelize map))
+
+(def pfilter! (parallelize filter))
+
+(def pmapcat! (parallelize mapcat))
